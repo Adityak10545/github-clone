fe1239cdc78491fd6ffe618e4ef19e9bc939a3b8
Adityak <adityakolekar47@gmail.com>
2024-04-12 13:46:00 +0530

    done with some changes

--- backend/db/connectMongoDB.js
+++ backend/db/connectMongoDB.js
@@ -1,19 +1,10 @@
 import mongoose from "mongoose";
 
-// MongoDB connection URI
-const mongoURI = "mongodb://localhost:27017/github-clone";
-
-// Connect to MongoDB
-mongoose.connect(mongoURI);
-
-// Check connection status
-const connectMongoDB = mongoose.connection;
-connectMongoDB.on(
-    "error",
-    console.error.bind(console, "MongoDB connection error:")
-);
-connectMongoDB.once("open", function () {
-    console.log("Connected to MongoDB");
-});
-
-export default connectMongoDB;
+export default async function connectMongoDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MONGODB connected");
+    } catch (error) {
+        console.log("Error connecting to MongoDB: ", error.message);
+    }
+}
